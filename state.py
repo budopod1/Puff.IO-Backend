c9601a5172b077e05728acb8fd72b7d893560b00
Martin Staab <51675874+budopod1@users.noreply.github.com>
2021-12-25 01:55:25 +0000

    The entire MMO state can be saved/loaded

--- state.py
+++ state.py
@@ -1,4 +1,6 @@
 from time import perf_counter
+from server import Server
+from user import User
 
 
 class State:
@@ -15,3 +17,17 @@ class State:
   def tick(self):
     self.time_delta = perf_counter() - self.last_tick
     self.last_tick = perf_counter()
+  
+  def save(self):
+    return {
+      "servers": [server.save() for server in self.servers],
+      "users": {username: user.save() for username, user in self.users.items()}
+    }
+  
+  @classmethod
+  def load(cls, data):
+    state = cls()
+    for server in data["servers"]:
+      state.add_server(Server.load(state, server))
+    for username, user in data["users"].items():
+      state.users[username] = User.load(state, user)
